8384205b929e9719c8621f5d6fcbc95287927c56
John Cameron <john.n.cameron@gmail.com>
2019-01-22 09:10:28 +1100

    requestRobots actions / reducer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider, connect } from 'react-redux';
-import { createStore } from 'redux';
-import { searchRobots } from './reducers';
+import thunkMiddleWare from 'redux-thunk';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { searchRobots, requestRobots } from './reducers';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import tachyons from 'tachyons';
 
-const store = createStore(
-  searchRobots,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const rootReducer = combineReducers({searchRobots, requestRobots})
+
+const store = 
+  createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleWare)));
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 
