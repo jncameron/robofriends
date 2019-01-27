fc65103d34e1fb6218d91b7bbc0c0c7cf71aeeea
John Cameron <john.n.cameron@gmail.com>
2019-01-27 19:17:06 +1100

    Updates

---
+++ precache-manifest.f5ea7ad3e64d488eda10cf4f520705f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "bfcf8f4cff0017b2f564",
+    "url": "/robofriends/static/js/main.bfcf8f4c.chunk.js"
+  },
+  {
+    "revision": "8d4363ccaee28efc1589",
+    "url": "/robofriends/static/js/1.8d4363cc.chunk.js"
+  },
+  {
+    "revision": "bfcf8f4cff0017b2f564",
+    "url": "/robofriends/static/css/main.264c6611.chunk.css"
+  },
+  {
+    "revision": "8d4363ccaee28efc1589",
+    "url": "/robofriends/static/css/1.e68504cb.chunk.css"
+  },
+  {
+    "revision": "bbef6cf939579d66ebd5ad147ced78af",
+    "url": "/robofriends/index.html"
+  }
+];
